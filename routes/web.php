7842cc30428b7da3f83fabb7c1048ff6c9ff7724
Your New Name <mahamudul408432@gmail.com>
2025-01-01 13:35:19 +0600

    Update dependencies and refactor layout structure for improved performance and maintainability

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
 
 Route::get('/dashboard', function () {
