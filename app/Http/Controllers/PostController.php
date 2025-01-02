f6fa05b11dcc4a69508c7380936953c7a30708a8
Your New Name <mahamudul408432@gmail.com>
2025-01-02 13:46:13 +0600

    Enhance admin post management view with dynamic post listing and improved styling

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view ('admin.index');
+        $posts = Post::all();
+        return view ('admin.index', compact('posts'));
     }
 
     /**
@@ -20,6 +21,7 @@ class PostController extends Controller
      */
     public function create()
     {
+        
         return view ('admin.create');
     }
 
