d9dcfb9e51d928ee371842a7640fee0567e0f7a2
Your New Name <mahamudul408432@gmail.com>
2025-01-03 20:33:14 +0600

    Add relationships to Comment, Post, and User models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+
+    protected $fillable = ['content'];
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['title', 'content'];
+
+
+
+
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
